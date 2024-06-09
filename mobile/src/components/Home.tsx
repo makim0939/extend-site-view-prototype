a86e14e743ecacd835ace1d5b3d96a85c468d457
makim0939 <95673492+makim0939@users.noreply.github.com>
2024-06-09 17:33:17 +0900

    Setup Tailwindcss

---
+++ mobile/src/components/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="">たこやき</h1>
+        <p>Portfolio</p>
+      </div>
+
+      <p>Welcome to the home page!</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ mobile/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        dark: '#252528',
+        light: '#757578',
+        lighter: '#d5d5d8',
+      },
+    },
+  },
+  plugins: [],
+};
