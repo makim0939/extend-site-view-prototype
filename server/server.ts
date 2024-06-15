0ab44644faa1436b3098d217a4939af405f28f6a
makim0939 <95673492+makim0939@users.noreply.github.com>
2024-06-15 17:10:35 +0900

    Update server/server.ts to setup socket.io server

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,15 @@
-console.log('Hello');
+import http from 'http';
+import { Server } from 'socket.io';
+
+const server = http.createServer();
+server.listen(3000, () => console.log('Server running on port 3000'));
+
+const io = new Server(server, {
+  cors: {
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('User connected', socket.client.request.headers.origin);
+});
