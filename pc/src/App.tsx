5235708b67ce89f23e06daf4f3c329541090ae02
makim0939 <95673492+makim0939@users.noreply.github.com>
2024-06-17 19:34:59 +0900

    Update pc/ to Setup socket.io client

--- pc/src/App.tsx
+++ pc/src/App.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
+import { io } from 'socket.io-client';
 import './App.css';
 
+const SERVER_URL = 'http://localhost:3000';
 function App() {
+  useEffect(() => {
+    const socket = io(SERVER_URL);
+    socket.on('connect', () => {
+      console.log('Connected to server');
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
   return (
     <>
       <h1>App - PC</h1>
